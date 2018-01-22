1084e830238c413cd3ba7e9110ced49cba49af92
Chris Whiting <chris.whiting@panasonic.aero>
2018-01-22 07:58:33 -0800

    Added redux form input, error checking and added styles to form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+      <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        {field.meta.error}
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title for Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   };
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content!";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
